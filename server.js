1975e7bd04b2cfecdf3f7d9ce28d0df85f12779e
dhanushramudri <ramudridhanush@gmail.com>
2024-03-15 21:05:05 +0530

    basic fetch

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.set("view engine ", "ejs");
 
 const PORT = process.env.PORT || 5000;
 
+const data = {
+  message: "This is a message",
+};
+
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(data);
 });
